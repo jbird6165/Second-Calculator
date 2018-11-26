927978d2f85a1dcad32892fad5ddc31ab177742f
Justin <jbird6165@gmail.com>
2018-11-26 15:00:44 -0600

    corrected syntax errors

--- cal.py
+++ cal.py
@@ -1,21 +1,24 @@
 import math
 
+#This is a test
+
 def number_input(n):
     while False:
         try:
             float(n)
             return n
         except ValueError:
-            n = print(input(("Not a Valid #."))
+            n = print(input(("Not a Valid #.")))
 
 
 
 def op_input(o):
-    l = [+, -, *, /, =]
-    for i in l:
-        if i != l:
-            o = print(input("Please enter + - * / or =."))
-        elif i == l:
+    while False:
+        l = ["+", "-", "*", "/", "="]
+        for i in l:
+            if i != l:
+                o = print(input("Please enter + - * / or =."))
+            elif i == l:
 
 
 def plus(rt, op):
