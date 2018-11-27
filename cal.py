c3dba1606a746c39303c111e3ff309885a1b74e3
Justin <jbird6165@gmail.com>
2018-11-26 21:30:10 -0600

    Cleaned up redundancies. Added another Function called "count". Eliminated extra coding at the bottom. Now when program runs it calls one function to start. Fixed division error.

--- cal.py
+++ cal.py
@@ -2,6 +2,44 @@
 it's not perfect but it's functional. I will continue to make updates as I learn.'''
 
 
+def Calculator(ent):
+    '''This is the loop that continues calculations until the "=" sign is entered.'''
+    rt = 0
+    while True:
+        num = number_input(input())
+        ent.append(num)
+        cnt = count(ent)
+        if cnt > 1:
+            rt = rt_equat(rt, op, num, ent)
+        else:
+            rt += num
+        op = op_input(input())
+        ent.append(op)
+        if op == "=":
+            total(rt, ent)
+            return
+
+
+def count(ent):
+    '''This will count how many entries there are so that we can get an
+    accurate running total after the first number input.'''
+    count = 0
+    for i in ent:
+        count += 1
+        if count == 2:
+            return count
+    return count
+
+
+def total(rt, ent):
+    if ent[1] == "=":
+        print(ent[0])
+    else:
+        print("Total: " + " " .join(str(x) for x in ent) + " " + str(round(rt, 10)))    # I need to figure out how to remove the .0 when a float returns a .0
+    ent.clear()
+    return
+
+
 def number_input(n):
     '''This will float the input and request input again if error occurs'''
     while True:
@@ -21,7 +59,7 @@ def op_input(o):
         o = input("Please enter + - * / or =:")
 
 
-def rt_equat(rt, op, num):
+def rt_equat(rt, op, num, ent):
     '''This takes the sign entered i.e. "+ - * / =" and returns the appropriate equation'''
     if op == "+":
         rt += num
@@ -33,47 +71,23 @@ def rt_equat(rt, op, num):
         rt *= num
         return rt
     elif op == "/":
+        num = zero_test(num, ent)
         rt /= num
         return rt
 
 
-def computations(rt, op, ent):
-    '''This is the loop that continues calculations until the "=" sign is entered.'''
-    while True:
-        if op != "=":
-            num = number_input(input())
-            rt = rt_equat(rt, op, num)
-            ent.append(num)
-            op = op_input(input())
-            ent.append(op)
-        else:
-            if ent[1] == "=":
-                print("Total: 0")
-                ent.clear()
-                rt = 0
-                return
-            else:
-                print("Total: " + "" .join(str(x) for x in ent) + str(round(rt, 10)))    # I need to figure out how to remove the .0 when a float returns a .0
-                ent.clear()                                                              # value. Haven't figured that out yet.
-                rt = 0
-            return
-
-
-def zero_test(n):
+def zero_test(num, ent):
     '''This tests the number to see if it's a 0. Will get a new number if it is.'''
     while True:
-        if n == 0:
-            n = number_input(input("Cannot Divide by 0. Please enter a valid #."))
+        if num == 0:
+            ent.pop()
+            num = number_input(input("Cannot Divide by 0. Please enter a valid #."))
+            ent.append(num)
         else:
-            return n
+            return num
 
 
 entries = []
-print("Welcome to Calculator 2.0.\n"
-"You can add, subtract, multiply, and divide!")
+print("Welcome to Calculator 2.0.\nYou can add, subtract, multiply, and divide!")
 while True:
-    run_total = number_input(input())
-    entries.append(run_total)
-    operator = (op_input(input()))
-    entries.append(operator)
-    computations(run_total, operator, entries)
+    Calculator(entries)
